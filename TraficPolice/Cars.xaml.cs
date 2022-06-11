ceed5d98416ff3abd7744db74d6f2f44c08858d4
LEONID <LEONID@DESKTOP-NPG5V15>
2022-06-11 17:30:55 +0800

    Добавьте файлы проекта.

---
+++ TraficPolice/Cars.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TraficPolice
+{
+    /// <summary>
+    /// Логика взаимодействия для Cars.xaml
+    /// </summary>
+    public partial class Cars : Page
+    {
+        public Cars()
+        {
+            InitializeComponent();
+        }
+
+        private void cmb_mark_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void txt_search_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            TrafficPoliceEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(entry => entry.Reload());
+            CarsList.ItemsSource = TrafficPoliceEntities.GetContext().Car.ToList();
+            var markList = TrafficPoliceEntities.GetContext().Car.ToList();
+            markList.Insert(0, new Car() { mark = "Все" });
+            cmb_mark.ItemsSource = markList;
+        }
+        public void RefreshData()
+        {
+            var list = TrafficPoliceEntities.GetContext().Car.ToList();
+            if (cmb_mark.SelectedIndex > 0)
+            {
+                Car car = cmb_mark.SelectedItem as Car;
+                list = list.Where(x=> x.mark == car.mark).ToList();
+            }
+            if(!string.IsNullOrWhiteSpace(txt_search.Text))
+            {
+                list = list.Where(x => x.StateNumber.ToLower().Contains(txt_search.Text.ToLower())).ToList();
+            }
+            CarsList.ItemsSource = list;
+        }
+    }
+}
---
+++ TraficPolice/MainWindow2.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TraficPolice
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow2.xaml
+    /// </summary>
+    public partial class MainWindow2 : Window
+    {
+        public MainWindow2()
+        {
+            InitializeComponent();
+            Manager.MainFrame = MainFrame;
+        }
+
+        private void BtnViolation_Click(object sender, RoutedEventArgs e)   
+        {
+            Manager.MainFrame.Navigate(new Violations());
+
+        } // Показать страницу про Нарушения
+
+        private void BntDriver_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new Drivers());
+
+        } // Показать страницу про Водителей
+
+        private void BtnCar_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new Cars());
+
+        } // Показать страницу про Автомобили
+
+        private void BtnGoback_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.GoBack();
+        } // Возвращение на предыдущую страницу
+
+        private void Frame_ContentRendered(object sender, EventArgs e)
+        {
+            if (MainFrame.CanGoBack)
+            {
+                BtnGoback.Visibility = Visibility.Visible;
+            }
+            else BtnGoback.Visibility = Visibility.Hidden;
+        } // Проверка возможности вернуться на предыдущую страницу
+    }
+}
